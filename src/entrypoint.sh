130d6e7592b9b2c21f03f86091c21f03a50a4e28
Ivan Lagunovsky <ivan.lagunovsky@gmail.com>
2025-01-11 12:21:02 +0100

    fix: add port and protocol to PUBLIC_DOMAIN setup in entrypoint script

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # fetch and setup IP
 if [ -z "$PUBLIC_DOMAIN" ]; then
-  PUBLIC_DOMAIN=$(curl -s https://checkip.amazonaws.com)
+  PUBLIC_DOMAIN=http://$(curl -s https://checkip.amazonaws.com):$PORT
   export PUBLIC_DOMAIN
   echo "setting PUBLIC_DOMAIN to $PUBLIC_DOMAIN"
 fi
